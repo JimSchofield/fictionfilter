31ab474eeebe04da7dff5cc9a9edf0321b28b5ad
Jim Schofield <oldcoyote@gmail.com>
2017-02-17 13:51:17 -0600

    Scripts set up with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var nodemon = require('gulp-nodemon');
+var concat = require('gulp-concat');  
+var rename = require('gulp-rename');  
+var uglify = require('gulp-uglify');  
 
 gulp.task('sass', function() {
 	gulp.src('styles/**/*.scss')
@@ -12,7 +15,17 @@ gulp.task('watch-sass', function() {
 	gulp.watch('./styles/**/*.scss',['sass']);
 });
 
-gulp.task('default',['watch-sass'], function() {
+gulp.task('jscripts', function() {
+	return gulp.src('js/**/*.js')
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('public/js'))
+});
+
+gulp.task('watch-jscripts', function() {
+	gulp.watch('./js/**/*.js', ['jscripts']);
+})
+
+gulp.task('default',['watch-sass', 'watch-jscripts'], function() {
 	return nodemon({
 		script: 'app.js',
 	});
