462529c3445d5d4ce622a416651fb98aa6afc27f
Jim Schofield <oldcoyote@gmail.com>
2017-02-21 17:29:52 -0600

    added error routing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes/routes');
+var apiRoutes = require('./api/index');
 
 var port = 3000;
 
@@ -13,6 +14,20 @@ app.set('views', __dirname + '/views')
 
 //setup routes
 app.use('/', routes);
+app.use('/api', apiRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error("404 Error: Not found, good sir.");
+	err.status = 404;
+	next(err);
+});
+
+//Error handler
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {title: "Error!", error: err.message})
+});
 
 
 app.listen(port, function() {
